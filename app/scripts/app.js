f020dc840f56a401b486821f2ea2443fb05be66d
Abhas Mittal <mittalabhas1@gmail.com>
2014-08-16 13:01:42 +0530

    fat(init): yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name smsChannelApp
+ * @description
+ * # smsChannelApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('smsChannelApp', [
+    'ngAnimate',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name smsChannelApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the smsChannelApp
+ */
+angular.module('smsChannelApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
